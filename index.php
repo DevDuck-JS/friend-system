03f81f17b7266bd20cb57f6856b9ad58d8d4a2c5
DevDuck-JS <103818112@student.swin.edu.au>
2024-10-16 16:04:08 +1100

    Build: Starter Setup * TailwindCSS * index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <title>My Friend System</title>
+</head>
+
+<body>
+  <h1 class="text-3xl bg-orange-800">My Friend System</h1>
+
+  <div class="">
+    <h1>My friend System</h1>
+    <h1>Assigment Home Page</h1>
+
+    <!-- Flex for Name & ID -->
+    <div>
+      <p>Name: Jinjuta Suksuwan</p>
+      <p>Student ID: 103818112</p>
+    </div>
+
+    <p>Email: 103818112@student.swin.edu.au</p>
+
+    <p>
+      I declare that this assignment is my individual work. I have not worked
+      collaboratively nor have I copied from any other student’s work or from
+      any other source.
+    </p>
+
+    <!-- Echo from PHP -->
+    <p>Table successfully created and populated</p>
+
+    <button class="bg-blue-500">Sign up</button>
+
+    <button class="bg-red-500">Log in</button>
+    <button>About</button>
+  </div>
+</body>
+
+</html>
